bf93d804a97dbab4402e5fa8fe379b02d45f1ba8
ReFil <31960031+ReFil@users.noreply.github.com>
2024-09-26 10:42:43 +0100

    4 bit ALU

--- top.v
+++ top.v
@@ -3,24 +3,29 @@ module top (input [3:0] SW, input P3_1, input P3_2, input P3_3, input P3_4, inpu
     wire F0, F1;
     wire X0, X1, X2, X3, Y0, Y1, Y2, Y3;
     wire OUT0, OUT1, OUT2, OUT3;
-    assign F0 = SW[0];
-    assign F1 = SW[1];
-    assign X3 = P1_1;
-    assign X2 = P1_2;
-    assign X1 = P1_3;
-    assign X0 = P1_4;
-    assign Y3 = P1_12;
-    assign Y2 = P1_11;
-    assign Y1 = P1_10;
-    assign Y0 = P1_9;
-    assign OUT3 = P2_4;
-    assign OUT2 = P2_3;
-    assign OUT1 = P2_2;
-    assign OUT0 = P2_1;
+    assign F0 = ~SW[0];
+    assign F1 = ~SW[1];
+    assign X3 = P3_1;
+    assign X2 = P3_2;
+    assign X1 = P3_3;
+    assign X0 = P3_4;
+    assign Y3 = P3_12;
+    assign Y2 = P3_11;
+    assign Y1 = P3_10;
+    assign Y0 = P3_9;
+    assign P2_1 = ~OUT0;
+    assign P2_2 = ~OUT1;
+    assign P2_3 = ~OUT2;
+    assign P2_4 = ~OUT3;
 
     four_bit_alu fba0(X0, X1, X2, X3, Y0, Y1, Y2, Y3, F0, F1, OUT0, OUT1, OUT2, OUT3);
+endmodule
 
-
+module four_bit_multiplexer(input X0, input X1, input X2, input X3, input Y0, input Y1, input Y2, input Y3, input MUX, output OUT0, output OUT1, output OUT2, output OUT3);
+    mux_cell m0 (X0, Y0, MUX, OUT0);
+    mux_cell m1 (X1, Y1, MUX, OUT1);
+    mux_cell m2 (X2, Y2, MUX, OUT2);
+    mux_cell m3 (X3, Y3, MUX, OUT3);
 endmodule
 
 module single_bit_adder_xor(input X, input Y, input Cin, input Sub, output XY, output Cout, output XOR );
@@ -50,12 +55,6 @@ module four_bit_adder_xor(input X0, input X1, input X2, input X3, input Y0, inpu
     single_bit_adder_xor a3 (.X(X3), .Y(Y3), .Cin(N_3), .Sub(F), .XY(XY3), .XOR(XOR3));
 endmodule
 
-module four_bit_multiplexer(input X0, input X1, input X2, input X3, input Y0, input Y1, input Y2, input Y3, input MUX, output OUT0, output OUT1, output OUT2, output OUT3);
-    mux_cell m0 (X0, Y0, MUX, OUT0);
-    mux_cell m1 (X1, Y1, MUX, OUT1);
-    mux_cell m2 (X2, Y2, MUX, OUT2);
-    mux_cell m3 (X3, Y3, MUX, OUT3);
-endmodule
 
 module four_bit_alu(input X0, input X1, input X2, input X3, input Y0, input Y1, input Y2, input Y3, input F0, input F1, output OUT0, output OUT1, output OUT2, output OUT3);
     wire XOR0;
@@ -71,4 +70,3 @@ module four_bit_alu(input X0, input X1, input X2, input X3, input Y0, input Y1, 
     four_bit_multiplexer fbm0 (XY0, XY1, XY2, XY3, XOR0, XOR1, XOR2, XOR3, F1, OUT0, OUT1, OUT2, OUT3);
 
 endmodule
-
